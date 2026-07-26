e2d04cf091e0d12366c34f18d7c40137cd70fb12
Seemant Shekhar <seemantshekhar43@gmail.com>
2026-07-26 20:54:32 +0530

    Add: coin-change - submission-4

---
+++ Data Structures & Algorithms/coin-change/submission-4.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        Integer[][] dp = new Integer[coins.length][amount + 1];
+        return traverse(0, amount, coins, dp);
+    }
+
+    public int traverse(int curr, int amount, int[] coins, Integer[][] dp){
+        // base case
+        if(amount == 0){
+            return 0;
+        }
+
+        // not possible case
+        if(amount < 0 || curr >= coins.length){
+            return -1;
+        }
+
+        // total coins required to get sum amount starting index curr
+        if(dp[curr][amount] != null){
+            return dp[curr][amount];
+        }
+
+        // including current with repeat
+        int incl = traverse(curr, amount - coins[curr], coins, dp);
+
+        //excluding current
+        int excl = traverse(curr + 1, amount, coins, dp);
+
+        int min = Integer.MAX_VALUE;
+
+        if(incl != -1 ){
+            min = Math.min(incl + 1,  min);
+        }
+
+        if(excl != -1){
+            min = Math.min(excl, min);
+        }
+
+        return dp[curr][amount] = (min == Integer.MAX_VALUE ? -1 : min);
+    }
+}
